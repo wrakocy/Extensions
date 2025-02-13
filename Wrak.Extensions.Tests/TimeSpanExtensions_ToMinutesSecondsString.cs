30f0419e96343e4d88127be953490834759885cf
Bill Rakocy <wrakocy@gmail.com>
2025-02-12 16:40:46 -0800

    Add ToMinutesSecond TimeSpan extension and bump version number to 1.3.0.

---
+++ Wrak.Extensions.Tests/TimeSpanExtensions_ToMinutesSecondsString.cs
@@ -0,0 +1,18 @@
+﻿namespace Wrak.Extensions.Tests;
+
+public class TimeSpanExtensions_ToMinutesSecondsString
+{
+    [Theory]
+    [InlineData(60, "60m 00s")]
+    [InlineData(90, "90m 00s")]
+    [InlineData(120, "120m 00s")]
+    public void Format(double minutes, string expectedString)
+    {
+        // Act
+        var result = TimeSpan.FromMinutes(minutes).ToMinutesSecondsString();
+
+        // Assert
+        Assert.Equal(expectedString, result);
+    }
+}
+
