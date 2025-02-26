c0fe646e5a4c0beb8ca4bf9685d005841a3ec2b5
Bill Rakocy <wrakocy@gmail.com>
2025-02-25 20:00:04 -0800

    Add ToYesNoString bool extension with tests,

---
+++ Wrak.Extensions.Tests/BoolExtensions_ToYesNoString.cs
@@ -0,0 +1,19 @@
+﻿namespace Wrak.Extensions.Tests;
+
+public class BoolExtensions_ToYesNoString
+{
+    [Theory]
+    [InlineData(true, "Yes")]
+    [InlineData(false, "No")]
+    public void ReturnYesOrNoFromBool(bool val, string expected)
+    {
+        Assert.Equal(expected, val.ToYesNoString());
+    }
+
+    [Fact]
+    public void HandleNull()
+    {
+        var val = (bool?)null;
+        Assert.Equal(string.Empty.ToStringOrEmpty(), val.ToYesNoString());
+    }
+}
---
+++ Wrak.Extensions/BoolExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Wrak.Extensions;
+
+public static class BoolExtensions
+{
+    public static string ToYesNoString(this bool val)
+    {
+        return val ? "Yes" : "No";
+    }
+
+    public static string ToYesNoString(this bool? val)
+    {
+        return val.HasValue ? val.Value.ToYesNoString() : "--";
+    }
+}
