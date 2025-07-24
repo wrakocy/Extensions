450420f34ef23da6107ee04a87b0bb038495c12b
Bill Rakocy <wrakocy@gmail.com>
2025-07-23 20:37:33 -0700

    Add ToGroupName Enum extension method with tests and increment package version number 1.6.0.

--- Wrak.Extensions.Tests/EnumExtensions_ToDisplayName.cs
+++ Wrak.Extensions.Tests/EnumExtensions_ToDisplayName.cs
@@ -5,11 +5,11 @@ namespace Wrak.Extensions.Tests;
 public class EnumExtensions_ToDisplayName
 {
     [Theory]
-    [InlineData(FakeEnum.Foo, "Foo", false)]
-    [InlineData(FakeEnum.Foo, "Foo", true)]
-    [InlineData(FakeEnum.FooBar, "Foo Bar", false)]
-    [InlineData(FakeEnum.FooBar, "FB", true)]
-    public void ReturnNameWithOrWithoutDisplayAnnotation(FakeEnum enumVal, string expectedResult, bool useShortName)
+    [InlineData(FakeDisplayNameEnum.Foo, "Foo", false)]
+    [InlineData(FakeDisplayNameEnum.Foo, "Foo", true)]
+    [InlineData(FakeDisplayNameEnum.FooBar, "Foo Bar", false)]
+    [InlineData(FakeDisplayNameEnum.FooBar, "FB", true)]
+    public void ReturnNameWithOrWithoutDisplayAnnotation(FakeDisplayNameEnum enumVal, string expectedResult, bool useShortName)
     {
         Assert.Equal(expectedResult, enumVal.ToDisplayName(useShortName));
     }
@@ -17,10 +17,10 @@ public class EnumExtensions_ToDisplayName
     [Fact]
     public void ReturnLongNameByDefault()
     {
-        Assert.Equal("Foo Bar", FakeEnum.FooBar.ToDisplayName());
+        Assert.Equal("Foo Bar", FakeDisplayNameEnum.FooBar.ToDisplayName());
     }
 
-    public enum FakeEnum
+    public enum FakeDisplayNameEnum
     {
         Foo,
 
---
+++ Wrak.Extensions.Tests/EnumExtensions_ToGroupName.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wrak.Extensions.Tests;
+
+public class EnumExtensions_ToGroupName
+{
+    [Theory]
+    [InlineData(FakeEnum.Foo, "")]
+    [InlineData(FakeEnum.Bar, "")]
+    [InlineData(FakeEnum.FooBar, "Bars")]
+    public void ReturnNameWithOrWithoutDisplayAnnotation(FakeEnum enumVal, string expectedResult)
+    {
+        Assert.Equal(expectedResult, enumVal.ToGroupName());
+    }
+
+    public enum FakeEnum
+    {
+        Foo,
+
+        [Display(Name = "Bar")]
+        Bar,
+
+        [Display(GroupName = "Bars")]
+        FooBar
+    }
+}
